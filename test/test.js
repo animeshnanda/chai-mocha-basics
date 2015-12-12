f38f927eb90ec6e27c55505a52f6ae6b7235673c
animeshnanda <animeshnanda@gmail.com>
2015-12-13 00:06:06 +0530

    Two test cases

--- test/test.js
+++ test/test.js
@@ -6,3 +6,9 @@
 			expect(true).to.be.true;
 		});
 	});
+	
+	describe('A basic test 2', function(){
+		it('should pass when everything is okay and awesome', function(){
+			expect(true).to.be.true;
+		});
+	});
